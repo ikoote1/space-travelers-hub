73effdc103018c853ddc1d86f0d4afdeb380a0eb
ikoote1 <ikootepreim@gmail.com>
2023-04-11 13:48:54 +0300

    navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,14 +6,14 @@ const Navigation = () => {
     return(
         <nav className="navBar">
             <ul className="navLogo">
-                <li><img src={logo} className="logo" alt="logo"></img></li>
-                <li><h1>Space Travelers' Hub</h1></li>
+                <li className="log"><img src={logo} className="logo" alt="logo"></img></li>
+                <li><h1 style={{fontSize: "30px"}}>Space Travelers' Hub</h1></li>
             </ul>
             <ul className="links">
-                <li><NavLink to="/" activeClassName="active" >Rockets</NavLink></li>
-                <li><NavLink to="/missions" activeClassName="active" >Missions</NavLink></li>
-                <li><div /></li>
-                <li><NavLink to="/profile" activeClassName="active">My Profile</NavLink></li>
+                <li><NavLink to="/" style={{color: "white", fontSize: "20px"}} activeClassName="active">Rockets</NavLink></li>
+                <li><NavLink to="/missions" style={{color: "white", fontSize: "20px"}} activeClassName="active">Missions</NavLink></li>
+                <li><div className="bar" /></li>
+                <li><NavLink to="/profile" style={{color: "white", fontSize: "20px"}} activeClassName="active">My Profile</NavLink></li>
             </ul>
         </nav>
     )
