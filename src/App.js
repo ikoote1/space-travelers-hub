6c05276513b39fd10f1d512bfbe9ef20b655c818
ikoote1 <ikootepreim@gmail.com>
2023-04-11 14:29:31 +0300

    rockets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/navigation';
+import Rockets from './components/rockets';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Routes>
-        <Route path="/" element={<rockets />} />
+        <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<missions />} />
         <Route path="/profile" element={<profile />} />
       </Routes>
