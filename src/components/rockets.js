95998bc86eee7d9f59a174695f95543ce877c971
ikoote1 <ikootepreim@gmail.com>
2023-04-11 14:27:48 +0300

    rockets

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -6,7 +6,27 @@ const Rockets = () => {
     return(
         <div className="rockets">
             <div className="rocket">
-                <div className="rocketImage"></div>
+                <div className="rocketImage"><img src={rocket1} alt="rocket"></img></div>
+                <div className="aboutR">
+                    <h1>Falcon1</h1>
+                    <p className="desc">The Rockets section displays a list of all
+                        available SpaceX rockets. Users can book each rocket by clicking the reservation
+                        button or cancel the previously made booking. The same layout is
+                        used to form the Dragons section</p>
+                    <button type="button">Reserve Rocket</button>
+                    <button type="button">Cancel Reservation</button>
+                </div>
+                <div className="rocketImage"><img src={rocket2} alt="rocket"></img></div>
+                <div className="aboutR">
+                    <h1>Falcon1</h1>
+                    <p className="desc">The Rockets section displays a list of all
+                        available SpaceX rockets. Users can book each rocket by clicking the reservation
+                        button or cancel the previously made booking. The same layout is
+                        used to form the Dragons section</p>
+                    <button type="button">Reserve Rocket</button>
+                    <button type="button">Cancel Reservation</button>
+                </div>
+                <div className="rocketImage"><img src={rocket1} alt="rocket"></img></div>
                 <div className="aboutR">
                     <h1>Falcon1</h1>
                     <p className="desc">The Rockets section displays a list of all
