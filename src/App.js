1592a0183828494b609f5e1a0b485e7bc9fe15b2
ikoote1 <ikootepreim@gmail.com>
2023-04-11 12:06:37 +0300

    seting up router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<rockets />} />
+        <Route path="/" element={<missions />} />
+        <Route path="/" element={<profile />} />
+      </Routes>
       <h1>GROUP</h1>
     </div>
   );
