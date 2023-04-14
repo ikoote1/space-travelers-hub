95a2646d8f81f65f7bf67635362321a91e0edc33
ikoote1 <ikootepreim@gmail.com>
2023-04-14 13:02:55 +0300

    finishing

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,20 +1,41 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import React from 'react';
 import logo from '../logo.png';
 
 const Navigation = () => (
-  <nav className="navBar">
-    <ul className="navLogo">
-      <li className="log"><img src={logo} className="logo" alt="logo" /></li>
-      <li><h1 style={{ fontSize: '30px' }}>Space Travelers` Hub</h1></li>
-    </ul>
-    <ul className="links">
-      <li><NavLink to="/" style={{ color: 'white', fontSize: '20px' }} activeClassName="active">Rockets</NavLink></li>
-      <li><NavLink to="/missions" style={{ color: 'white', fontSize: '20px' }} activeClassName="active">Missions</NavLink></li>
-      <li><div className="bar" /></li>
-      <li><NavLink to="/profile" style={{ color: 'white', fontSize: '20px' }} activeClassName="active">My Profile</NavLink></li>
-    </ul>
-  </nav>
+  // <nav className="navBar">
+  //   <ul className="navLogo">
+  //     <li className="log"><img src={logo} className="logo" alt="logo" /></li>
+  //     <li><h1 style={{ fontSize: '30px' }}>Space Travelers` Hub</h1></li>
+  //   </ul>
+  //   <ul className="links">
+  //     <li><NavLink to="/" style={{ color: 'white', fontSize: '20px' }}
+  //      activeClassName="active">Rockets</NavLink></li>
+  //     <li><NavLink to="/missions" style={{ color: 'white', fontSize: '20px' }}
+  //      activeClassName="active">Missions</NavLink></li>
+  //     <li><div className="bar" /></li>
+  //     <li><NavLink to="/profile" style={{ color: 'white', fontSize: '20px' }}
+  //      activeClassName="active">My Profile</NavLink></li>
+  //   </ul>
+  // </nav>
+  <>
+    <nav>
+      <div className="logo">
+        <img src={logo} alt="space-logo" />
+        <h1>Space Travelers&#39; Hub</h1>
+      </div>
+      <ul>
+        <NavLink to="/">Rockets</NavLink>
+        <NavLink to="Dragons">Dragons</NavLink>
+        <NavLink to="mission">Mission</NavLink>
+        <hr />
+        <NavLink to="my_profile">My Profile</NavLink>
+      </ul>
+    </nav>
+    <main>
+      <Outlet />
+    </main>
+  </>
 );
 
 export default Navigation;
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 const Profile = ({ missionprofile }) => (
-  const { rocketStore } = useSelector((store) => store.rocket);
+  // const { rocketStore } = useSelector((store) => store.rocket);
 
   <div className="container">
     <div className="row">
@@ -21,17 +21,17 @@ const Profile = ({ missionprofile }) => (
       </div>
       <div className="col-6">
         <h2>My Rockets</h2>
-        <table className="table table-bordered">
-          <tbody>
-          {rocketStore
-                .filter((rocket) => rocket.reserved)
-                .map((reservedRocket) => (
-                  <tr key={reservedRocket.id}>
-                    <th className="mprtd">{reservedRocket.name}</th>
-                  </tr>
-                ))}
+        <table className="table table-bordered" />
+        {/* <tbody>
+            {rocketStore
+              .filter((rocket) => rocket.reserved)
+              .map((reservedRocket) => (
+                <tr key={reservedRocket.id}>
+                  <th className="mprtd">{reservedRocket.name}</th>
+                </tr>
+              ))}
           </tbody>
-        </table>
+        </table> */}
       </div>
     </div>
   </div>
